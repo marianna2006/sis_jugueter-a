18e504eb061f430e38f9d1deb0a4716a050aa858
Marianna Mori <1631985@senati.pe>
2025-10-29 11:02:09 -0500

    feat:Creando configuración inicial de React

---
+++ src/frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+     return(
+          <div className="min-h-screen bg-gray-100 p-8">
+               <h1 className="text-2xl font-bold mb-4">Bienvenido(a) a mi página principal 🏠</h1>
+               <p className="text-green-600 text-xl">Esta es nuestra aplicación con React.</p>
+          </div>
+     );
+}
+
+export default Home;
