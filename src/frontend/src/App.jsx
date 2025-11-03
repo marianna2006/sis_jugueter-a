10c1e752d17ff67af24e589cb144b42c026babc6
Marianna Mori <1631985@senati.pe>
2025-11-03 11:29:54 -0500

    feat:Enviando vista de Login y Success

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import  Home  from "./pages/Home";
+import Login from "./pages/Login";
+import LoginSuccess from "./pages/LoginSuccess";
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/login-sucess" element={<LoginSuccess />} />
       </Routes>
     </BrowserRouter>
   );
